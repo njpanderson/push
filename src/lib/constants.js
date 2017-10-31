db0c209a9530c15e9352e0791f0305ef06d83ad6
Neil Anderson <njp.anderson@icloud.com>
2017-10-31 19:56:00 +0000

    refactor service config to be more readable

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,10 @@
 module.exports = {
-    DEFAULT_SERVICE_CONFIG: '{\n\t"service": "[ServiceName]",\n' +
-        '\t"[ServiceName]": {\n\t\t\/\/ Add service configuration here...\n\t}\n}'
+    DEFAULT_SERVICE_CONFIG: [
+        '{',
+            '\t"service": "[ServiceName]",',
+            '\t"[ServiceName]": {',
+                '\t\t\/\/ Add service configuration here...',
+            '\t}',
+        '}'
+    ].join('\n')
 };
