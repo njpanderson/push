d76dd05e2f94ee526c42c152a4111138e99f1f1b
Neil Anderson <njp.anderson@icloud.com>
2019-02-09 19:38:16 +0000

    Initial work on settings UI; asset resolution

--- src/Service/ServiceSettings/SettingsUI.js
+++ src/Service/ServiceSettings/SettingsUI.js
@@ -1,22 +1,32 @@
+const vscode = require('vscode');
+
+const utils = require('../../lib/utils');
+
 class SettingsUI {
-	show() {
-		this.panel = vscode.window.createWebviewPanel(
-			'push.serviceSettings',
-			'Push Service Settings',
-			vscode.ViewColumn.One,
-			{
-				enableScripts: true
-			}
-		);
+	show(settingsFile) {
+		return new Promise((resolve, reject) => {
+			// TODO: rejection if the file doesn't exist?
+			this.panel = vscode.window.createWebviewPanel(
+				'push.serviceSettings',
+				'Push Service Settings',
+				vscode.ViewColumn.One,
+				{
+					enableScripts: true
+				}
+			);
 
-		this.panel.webview.html = ;
+			// Set panel content
+			this.panel.webview.html = utils.getAsset('service-ui/index.html');
 
-		this.panel.webview.onDidReceiveMessage((event) => {
-			console.log('message from webview!');
-			console.log(event);
+			this.panel.webview.onDidReceiveMessage((event) => {
+				console.log('message from webview!');
+				console.log(event);
+			});
+
+			this.panel.webview.postMessage('hello webview!');
+
+			resolve();
 		});
-
-		this.panel.webview.postMessage('hello webview!');
 	}
 }
 
