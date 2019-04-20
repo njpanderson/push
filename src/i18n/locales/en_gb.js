aad821f1d15f228407ec9cd81f4f45a3c20bef17
Neil Anderson <njp.anderson@icloud.com>
2019-04-20 20:39:32 +0100

    Fill in more it translations

--- src/i18n/locales/en_gb.js
+++ src/i18n/locales/en_gb.js
@@ -90,7 +90,7 @@ module.exports = {
 	'stream_read': 'Stream IO read error for file "${1}". Error: ${2}',
 	'stream_write': 'Stream IO write error for file "${1}". Error: ${2}',
 	'cannot_list_directory': 'Directory "${1}" cannot be listed: ${2}',
-	'could_not_load_gateway_key': 'Could not load a private key file on the gateway at "${1}". Server error was: ${2}`',
+	'could_not_load_gateway_key': 'Could not load a private key file on the gateway at "${1}". Server error was: ${2}',
 	'error_from_gateway': 'Error occured on SSH gateway: "${1}"',
 	'invalid_uri_scheme': 'Sorry, the filesystem scheme "${1}" is not supported. Push cannot work on files of this type.',
 	'invalid_path': 'The path "${1}" does not appear to be valid and has been skipped.',
